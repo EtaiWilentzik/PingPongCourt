56e8216779402a198ac2b006bbb6dc8ae3efafdb
adital2512 <adi.tal.25.12@gmail.com>
2024-11-22 22:10:23 +0200

    changed to table in stats

--- Client/my-app/src/Screens/Statistics/GameStats.js
+++ Client/my-app/src/Screens/Statistics/GameStats.js
@@ -141,8 +141,8 @@ export function GameStats({ gameId }) {
             <td className="stats-column">
               <div>
                 <video
+                  className="large-video"
                   id="videoPlayer"
-                  width="650"
                   controls
                   muted="muted"
                   autoPlay
